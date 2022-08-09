7fd7c2a5968aaf8ab66d087a43bc64723a453d01
toMilanesi <tomi.milanesi@gmail.com>
2022-08-09 11:44:42 -0300

    Added second query

--- queries.sql
+++ queries.sql
@@ -1,3 +1,4 @@
 /*Queries that provide answers to the questions from all projects.*/
 
 SELECT * from animals WHERE name LIKE '%mon';
+SELECT * from animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
