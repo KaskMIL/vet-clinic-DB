9429c2a662d7d086a84a9cfbd99243c74574f5fe
toMilanesi <tomi.milanesi@gmail.com>
2022-08-09 10:52:18 -0300

    Insert first animal

--- data.sql
+++ data.sql
@@ -1,5 +1,5 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Augmon', '2020-02-03', 0, TRUE, 10.23);
 INSERT INTO animals (name) VALUES ('Daisy');
 INSERT INTO animals (name) VALUES ('Charlie');
