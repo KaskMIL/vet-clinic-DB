b3c6d3668b7e3fb24f1060f212ca4458d51b2990
toMilanesi <tomi.milanesi@gmail.com>
2022-08-09 10:57:22 -0300

    Added third animal

--- data.sql
+++ data.sql
@@ -2,4 +2,4 @@
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Augmon', '2020-02-03', 0, TRUE, 10.23);
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Gabumon', '2018-11-15', 2, TRUE, 8);
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Pikachu', '2021-01-07', 1, FALSE, 15.04);
