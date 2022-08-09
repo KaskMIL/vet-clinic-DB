d95b4b9640659951ab5fffd5b049b1eb3a821db6
toMilanesi <tomi.milanesi@gmail.com>
2022-08-09 11:50:38 -0300

    Added third query

--- queries.sql
+++ queries.sql
@@ -1,4 +1,5 @@
 /*Queries that provide answers to the questions from all projects.*/
 
 SELECT * from animals WHERE name LIKE '%mon';
-SELECT * from animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
+SELECT name from animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
+SELECT name from animals WHERE neutered = TRUE AND escape_attempts < 3;
