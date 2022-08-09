4b18276008e7582bbe7518c9e3a724377dca77a1
toMilanesi <tomi.milanesi@gmail.com>
2022-08-09 12:00:16 -0300

    Added sixth query

--- queries.sql
+++ queries.sql
@@ -5,3 +5,4 @@ SELECT name from animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-3
 SELECT name from animals WHERE neutered = TRUE AND escape_attempts < 3;
 SELECT date_of_birth FROM animals WHERE name = 'Augmon' OR name =  'Pikachu';
 SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
+SELECT * FROM animals WHERE neutered = TRUE;
