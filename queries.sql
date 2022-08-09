0dbf639512cfcf29b9e067c88515fa323194710c
toMilanesi <tomi.milanesi@gmail.com>
2022-08-09 11:54:24 -0300

    Added fourth  query

--- queries.sql
+++ queries.sql
@@ -3,3 +3,4 @@
 SELECT * from animals WHERE name LIKE '%mon';
 SELECT name from animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
 SELECT name from animals WHERE neutered = TRUE AND escape_attempts < 3;
+SELECT date_of_birth FROM animals WHERE name = 'Augmon' OR name =  'Pikachu';
